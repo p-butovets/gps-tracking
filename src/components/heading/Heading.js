c2cf16119f17473cd2661eaa2d4f58004747e0a1
Butovets Pavlo <p.butovets@gmail.com>
2022-10-27 05:47:39 +0300

    add some css fixes

--- src/components/heading/Heading.js
+++ src/components/heading/Heading.js
@@ -2,7 +2,9 @@ import './heading.scss';
 
 const Heading = (props) => {
     return (
-        <section className="heading">{props.text}</section>
+        <section className="heading">
+            {props.text}<span className="version">v0.1.0</span>
+        </section>
     )
 }
 
